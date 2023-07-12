16f50652a012e26509caaaea58831414cbf33f61
Jeffrey <thelostpotato27@gmail.com>
2023-07-11 17:02:33 -0700

    Lambda func that initializes ECS service taht hosts the server

---
+++ lambda.py
@@ -0,0 +1,27 @@
+import boto3
+
+REGION = 'us-east-2'
+CLUSTER = 'minecraft'
+SERVICE = 'minecraft-server'
+
+
+def lambda_handler(event, context):
+    """Updates the desired count for a service."""
+
+    ecs = boto3.client('ecs', region_name=REGION)
+    response = ecs.describe_services(
+        cluster=CLUSTER,
+        services=[SERVICE],
+    )
+
+    desired = response["services"][0]["desiredCount"]
+
+    if desired == 0:
+        ecs.update_service(
+            cluster=CLUSTER,
+            service=SERVICE,
+            desiredCount=1,
+        )
+        print("Updated desiredCount to 1")
+    else:
+        print("desiredCount already at 1")
